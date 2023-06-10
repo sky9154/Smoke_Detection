3a793c366abb0e7befa78b603901ac4e42d39760
sky9154 <sky0932039612@gmail.com>
2023-06-11 00:14:58 +0800

    更改 API 地址

--- Interface/src/api/report.ts
+++ Interface/src/api/report.ts
@@ -1,5 +1,5 @@
 const report = async (message: string, image: File) => {
-  const url = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/report`;
+  const url = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/api/report/upload`;
   const formData = new FormData();
 
   formData.append('message', message);
